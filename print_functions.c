292ba943e2bf0f559dbaa4219cda24175b82e0d5
Thays Ribeiro <thaysribeiros@gmail.com>
2021-08-30 19:04:03 -0700

    Create print_functions.c

---
+++ print_functions.c
@@ -0,0 +1,39 @@
+#include "my_mastermind.h"
+
+void print_first_round(int round, struct init_options *options, int attempts)
+{
+    printf("Will you find the secret code?\n");
+    printf("You have %d attempts, good luck!\n", attempts);
+    printf("---\n");
+    printf("Round %d\n", round);
+}
+
+void print_rounds(int round, struct init_options *options)
+{
+    printf("---\n");
+    printf("Round %d\n", round);
+}
+
+void print_secret_code(struct init_options *options)
+{
+    if (options->c == false)
+    {
+        printf("The secret code was: --> %d <--\nGood luck next time!\n", options->random_code);
+    }
+    else
+    {
+        printf("The secret code was: --> %s <--\nGood luck next time!\n", options->code);
+    }
+}
+
+void print_bool(bool option)
+{
+    if (option == true)
+    {
+        printf("true\n");
+    }
+    else
+    {
+        printf("false\n");
+    }
+}
