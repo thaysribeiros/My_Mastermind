64526894041f9016843fe928865566aaa7f537bf
Thays Ribeiro <thaysribeiros@gmail.com>
2021-09-01 14:09:05 -0700

    Update my_mastermind.c
    Improved return statement in main

--- my_mastermind.c
+++ my_mastermind.c
@@ -8,10 +8,10 @@ int main(int ac, char** av)
    char code[128];
    int attempts;
 
-   if (valid_option == EXIT_SUCCESS)
+   if (valid_option == 0)
    {
             attempts = number_of_attempts(&options);
-            print_first_round(round, &options, attempts);
+            print_first_round(round, attempts);
             while (1)
             {
                 read(0, code, sizeof(code));
@@ -20,26 +20,26 @@ int main(int ac, char** av)
                 {
                     if (check_code(code, &options) == EXIT_SUCCESS)
                     {
-                        return 0;
+                        return EXIT_SUCCESS;
                     }
                 }
                 else
                 {
                     printf("Code should be 4 digits in range [0 to 7]\nPlease, start again!\n");
-                    return -1; 
+                    return EXIT_FAILURE; 
                 }
                 if (round >= attempts)
                 {
                     printf("KO! Game over!\n");
                     print_secret_code(&options);
-                    return 0;
+                    return EXIT_SUCCESS;
                 }
-                print_rounds(round, &options);
+                print_rounds(round);
             }
    }
    else
    {
         printf("My_Mastermind accepts only: [-c] + a code of 4 digits in range [0 to 7] + [-t] + a number bigger than zero.\nExample: ./my_mastermind -c 1234 -t 8\n");
    }
-   return 0;
+   return EXIT_SUCCESS;
 }
